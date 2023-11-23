36cf20003a4cd5838016bf19a41202b8e532dd17
Aman <127581729+Aman5701@users.noreply.github.com>
2023-11-23 17:07:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ $(document).ready(function(){
     });
 
 });
+
+
+
+
+// Animate on Scroll
+
+let sections = document.querySelectorAll('section');
+
+window.onscroll=()=> {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+    });
+}
